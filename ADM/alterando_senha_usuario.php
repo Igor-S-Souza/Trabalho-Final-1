23e1ca6a198ac261698707dc8adb9d40ce24f255
Igor souza <89223876+lulamolusc0@users.noreply.github.com>
2022-05-29 15:10:16 -0300

    ajustando mudança de senha

--- ADM/alterando_senha_usuario.php
+++ ADM/alterando_senha_usuario.php
@@ -12,7 +12,7 @@ $conf_senha = $_POST['conf_senha'];
 //verificando se as duas senhas são iguais
 if ($senha == $conf_senha) {
 	//alterando a senha do usuário
-	$alterando = mysqli_query($conexao,"UPDATE usuario_por_setor SET senha = '$conf_senha'");
+	$alterando = mysqli_query($conexao,"UPDATE usuario_por_setor SET senha = '$conf_senha' WHERE id ='$id' ");
 	//se a senha foi inserida com sucesso
 	if (isset($alterando)){
 		$_SESSION['msg_senha_sucesso'] = "<p class ''>Senha alterada com sucesso!</p>";
