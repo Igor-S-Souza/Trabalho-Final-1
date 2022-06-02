176f6cfd9132e55c8c98c2f3940074ef3ade8e67
raisena00 <104871566+raisena00@users.noreply.github.com>
2022-06-02 20:27:42 -0300

    css editar usuario

--- ADM/editar_usuario.php
+++ ADM/editar_usuario.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 include ("../DADOS_USUARIO/dados_de_usuario.php");
 
@@ -11,10 +11,11 @@ $id = $_GET['id'];
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="../CSS/estilo.css">
 	<title></title>
 </head>
-<body>
-<?php 
+<body class="corpo">
+<?php
 $selecionando = mysqli_query($conexao, "SELECT * FROM usuario_por_setor WHERE id ='$id'");
 while ($campo = mysqli_fetch_array($selecionando)) {
 	$login = $campo['login'];
@@ -23,16 +24,21 @@ while ($campo = mysqli_fetch_array($selecionando)) {
 	$contato = $campo['contato'];
 }
  ?>
+ <center>
+ <div class="editar_usuario">
 <form method="POST" action="editando_usuario.php?id=<?php echo $id ?>">
-	<label>Login:</label><br>
+  <br>
+	<label class="titulo_senha">Login:</label><br>
 	<input type="text" name="login" value="<?php echo $login; ?>" disabled><br><br>
-	<label>Nome:</label><br>
+	<label class="titulo_senha">Nome:</label><br>
 	<input type="text" name="nome" value="<?php echo $nome; ?>"><br><br>
-	<label>Setor:</label><br>
+	<label class="titulo_senha">Setor:</label><br>
 	<input type="text" name="setor" value="<?php echo $setor; ?>"><br><br>
-	<label>Contato:</label><br>
+	<label class="titulo_senha">Contato:</label><br>
 	<input type="text" name="contato" value="<?php echo $contato; ?>"><br><br>
-	<input type="submit" name="enviar">
+	<input type="submit" name="enviar" class="submit_senha">
+</div>
+</center>
 </form>
 </body>
 </html>
